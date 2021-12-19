2bd49d74f234cd2f24b29dc84cde984531d99c5a
Abel Souza <abelsouzacosta@gmail.com>
2021-12-19 03:28:47 -0300

    feat(checksExistsUserAccount): adding middleware
    Verifica se o usuário passado no header da requisição realmente existe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { v4: uuid } = require('uuid');
+const res = require('express/lib/response');
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -8,14 +10,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user)
+    return response.status(404).json({
+      error: "User not found"
+    });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const id = uuid();
+
+  const user = {
+    id,
+    name,
+    username,
+    todos: []
+  }
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
